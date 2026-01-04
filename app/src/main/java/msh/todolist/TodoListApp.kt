06c7e092acafe721408527598122b51947bf582f
maurospagnoliherrera <maurospagnoliherrera@gmail.com>
2026-01-05 00:42:20 +0100

    Removed unused code

--- app/src/main/java/msh/todolist/TodoListApp.kt
+++ app/src/main/java/msh/todolist/TodoListApp.kt
@@ -4,18 +4,7 @@ import android.app.Application
 import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 import msh.todolist.constants.Constants.LOG_TAG
-import msh.todolist.initialization.InitializationManager
 import javax.inject.Inject
 
 @HiltAndroidApp
-class TodoListApp : Application() {
-
-    @Inject
-    lateinit var initializationManager: InitializationManager
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.d(LOG_TAG, "TodoListApp onCreate - Initialization started")
-        initializationManager.initialize()
-    }
-}
+class TodoListApp : Application() 
